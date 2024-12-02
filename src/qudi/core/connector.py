13df6fe85f03c6492c24d6e46e2f5a10497bdc1e
wkrokosz <w.krokosz@cent.uw.edu.pl>
2024-12-02 13:04:50 +0100

    Strongly type connector instances
    Fix runtime typing issues

--- src/qudi/core/connector.py
+++ src/qudi/core/connector.py
@@ -22,16 +22,20 @@ If not, see <https://www.gnu.org/licenses/>.
 __all__ = ['Connector']
 
 import weakref
-from typing import Any, Type, Union
+from typing import TYPE_CHECKING, Any, Union, TypeVar, Generic
 from qudi.util.overload import OverloadProxy
+if TYPE_CHECKING:
+    from qudi.core.module import Base
+    T = TypeVar('T', bound=Base)
+else:
+    T = TypeVar('T')
 
-
-class Connector:
+class Connector(Generic[T]):
     """A connector used to connect qudi modules with each other.
     """
 
     def __init__(
-        self, interface: Union[str, Type], name: str = None, optional: bool = False
+        self, interface: Union[str, type[T]], name: str = None, optional: bool = False
     ):
         """Initialize a Connector instance.
 
@@ -66,7 +70,7 @@ class Connector:
         if self.name is None:
             self.name = name
 
-    def __call__(self) -> Any:
+    def __call__(self) -> T:
         """Return reference to the module that this connector is connected to."""
         if self.is_connected:
             return self._obj_proxy
